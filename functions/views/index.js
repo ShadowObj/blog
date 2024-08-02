db00b3c3603d0dd78c1339168b389187988f1696
Shadowobj <shadowobject@protonmail.com>
2024-08-02 18:10:40 +0800

    Just the last try...

--- functions/views/index.js
+++ functions/views/index.js
@@ -1,9 +1,8 @@
 export async function onRequestGet(context) {
     const pathname = decodeURI(new URL(context.request.url).searchParams.get("q"))
-    if (!(pathname.startsWith("/")) || pathname.includes('"')) {
+    if (!(pathname.startsWith("/")) || pathname.includes("'")) {
         return new Response("403 Forbidden", {"status":403})
     }
-    var originData = {}
     var originData = await context.env.BLOG_DB.prepare('SELECT * FROM views LIMIT 1').first();
 
     var ps;
@@ -11,13 +10,13 @@ export async function onRequestGet(context) {
         (typeof originData[pathname] == null) || 
         (typeof originData[pathname] == undefined)
     ) {
-        ps = context.env.BLOG_DB.prepare('UPDATE views SET ?1 = ?2'
+        ps = context.env.BLOG_DB.prepare("UPDATE views SET '?1' = ?2"
             .replace("?1",pathname)
             .replace("?2",parseInt(originData[pathname])+1)
         );
         originData[pathname] = parseInt(originData[pathname])+1;
     } else {
-        ps = context.env.BLOG_DB.prepare('ALTER TABLE views ADD ?1 INTEGER DEFAULT 1'
+        ps = context.env.BLOG_DB.prepare("ALTER TABLE views ADD '?1' INTEGER NOT NULL DEFAULT 1"
             .replace("?1",pathname)
         );
         originData[pathname] = 1;
