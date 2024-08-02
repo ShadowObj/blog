3b89f757d9f328cdc3ce4a2b309f9430a13528f0
Shadowobj <shadowobject@protonmail.com>
2024-08-02 15:45:59 +0800

    Added ViewsCount & functions

--- functions/views/get.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestGet(context) {
-    const ps = context.env.BLOG_DB.prepare('SELECT * FROM views LIMIT 1');
-    const data = await ps.first();
-  
-    return Response.json(data);
-}
