e2fd6dacf5daea7845518487d062c56385293b0e
Shadowobj <shadowobject@protonmail.com>
2024-08-02 16:42:33 +0800

    One Last...Fix...

--- functions/views/index.js
+++ functions/views/index.js
@@ -1,25 +1,25 @@
 export async function onRequestGet(context) {
     const pathname = decodeURI(new URL(context.request.url).searchParams.get("q"))
-    if (pathname == null) {
+    if (!(pathname.startsWith("/")) || pathname.includes('"')) {
         return new Response("403 Forbidden", {"status":403})
     }
-    const originPs = context.env.BLOG_DB.prepare('SELECT * FROM views LIMIT 1');
-    const originData = await originPs.first();
+    const originPS = context.env.BLOG_DB.prepare('SELECT * FROM views LIMIT 1');
+    const originData = await originPS.first();
 
-    var result = false;
     var ps;
     if (originData.hasOwnProperty(pathname)) {
         ps = context.env.BLOG_DB.prepare('UPDATE views SET ?1 = ?2'
-            .replace("?1",pathname.replace("/","//"))
+            .replace("?1",pathname)
             .replace("?2",parseInt(originData[pathname])+1)
         );
+        originData[pathname] = parseInt(originData[pathname])+1;
     } else {
         ps = context.env.BLOG_DB.prepare('ALTER TABLE views ADD ?1 INTEGER NOT NULL DEFAULT 1'
-            .replace("?1",pathname.replace("/","//"))
+            .replace("?1",pathname)
         );
+        originData[pathname] = 1;
     }
 
     result = await ps.all();
-    originData[pathname] = parseInt(originData[pathname])+1;
     return Response.json(originData);
 }
