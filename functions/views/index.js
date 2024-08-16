6a9b9adf68eaf0d662610927c10aff74a4e5b0b9
Shadowobj <shadowobject@protonmail.com>
2024-08-16 18:56:17 +0800

    function viewsCount updated

--- functions/views/index.js
+++ functions/views/index.js
@@ -1,4 +1,4 @@
-export async function onRequestPost(context) {
+export async function onRequestGet(context) {
     const pathname = decodeURI(new URL(context.request.url).searchParams.get("q"))
     if (!(pathname.startsWith("/")) || pathname.includes("'")) {
         return new Response("403 Forbidden", {"status":403})
@@ -13,11 +13,11 @@ export async function onRequestPost(context) {
         ps = context.env.BLOG_DB.prepare(`ALTER TABLE views ADD '${pathname}' INTEGER NOT NULL DEFAULT 1`);
         originData[pathname] = 1;
     }
-    await ps.all();
+    context.waitUntil(new Promise(() => ps.all()));
 
     let total = Object.values(originData).reduce((acr, cur) => {return acr + cur;});
     if ((total%100) == 0) {
-        await context.env.BLOG_DB.prepare(`ALTER TABLE views_total ADD '${total}' INTEGER NOT NULL DEFAULT ${Date.now()}`).all();
+        context.waitUntil(new Promise(() => context.env.BLOG_DB.prepare(`ALTER TABLE views_total ADD '${total}' INTEGER NOT NULL DEFAULT ${Date.now()}`).all()));
     }
     return Response.json(originData);
 }
